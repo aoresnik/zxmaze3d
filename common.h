f01e1f558d68135fa8bc05d6fa75e5bd22e55ae1
Andrej <andrej5173@gmail.com>
2015-08-05 23:14:36 +0200

    Separated interrupt setup code to irq.c.

--- common.h
+++ common.h
@@ -8,3 +8,15 @@
 #define debug_printf printf
 
 #endif //NDEBUG
+
+// Statically allocated address ranges
+
+// 0xF000 - 0xF7FF (2048 bytes)
+#define ADDR_ALIGNED_FSQRT_LUT		0xF000
+
+// 0xFDFD - 0xFDFF (3 bytes)
+#define ADDR_IRQ_TRAMPOLINE			0xFDFD
+// 0xFE00 - 0xFF01 (257 bytes)
+#define ADDR_IRQ_VECTOR_TABLE		0xFE00
+
+
